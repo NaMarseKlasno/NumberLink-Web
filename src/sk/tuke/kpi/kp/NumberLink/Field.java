594dc1e9c715d3f25a4486e8aa1f88be3fc97a09
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2022-02-20 20:42:23 +0100

    project initialization

--- src/sk/tuke/kpi/kp/NumberLink/Field.java
+++ src/sk/tuke/kpi/kp/NumberLink/Field.java
@@ -5,5 +5,4 @@ public class Field {
     public Field() {
 
     }
-
 }
--- src/sk/tuke/kpi/kp/NumberLink/Main.java
+++ src/sk/tuke/kpi/kp/NumberLink/Main.java
@@ -1,9 +1,8 @@
 package sk.tuke.kpi.kp.NumberLink;
 
+import sk.tuke.kpi.kp.NumberLink.Field;
+
 public class Main {
     public static void main(String[] args) {
-        return;
     }
-
-
 }
